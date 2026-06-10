9199630289bc80bd0191ec0f3443ac4db9dffcf1
LuisMartinezM98 <104807270+LuisMartinezM98@users.noreply.github.com>
2026-06-10 09:25:27 -0600

    feat: create expense action implemented

---
+++ src/actions/create-expense-action.ts
@@ -0,0 +1,68 @@
+"use server";
+
+import getToken from "@/auth/token";
+import {
+  DraftExpenseSchema,
+  ErrorResponseSchema,
+  SuccessSchema,
+} from "@/schemas";
+import { revalidatePath } from "next/cache";
+
+type ActionStateType = {
+  errors: string[];
+  success: string;
+};
+
+export default async function createExpense(
+  budgetId: number,
+  prevSate: ActionStateType,
+  formData: FormData,
+) {
+  const expenseData = {
+    name: formData.get("name"),
+    amount: formData.get("amount"),
+  };
+
+  const expense = DraftExpenseSchema.safeParse(expenseData);
+  if (!expense.success) {
+    return {
+      errors: expense.error.issues.map((issue) => issue.message),
+      success: "",
+    };
+  }
+
+  //Generar gasto
+  const token = await getToken();
+  const url = `${process.env.NEXT_PUBLIC_API_URL}/api/budgets/${budgetId}/expenses`;
+  const req = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({
+      name: expense.data.name,
+      amount: expense.data.amount,
+    }),
+  });
+
+  const json = await req.json();
+
+  if (!req.ok) {
+    const { error } = ErrorResponseSchema.parse(json);
+    return {
+      errors: [error],
+      success: "",
+    };
+  }
+
+  const success = SuccessSchema.parse(json);
+
+  revalidatePath(`/admin/budgets/${budgetId}`);
+
+
+  return {
+    errors: [],
+    success: success.message,
+  };
+}
