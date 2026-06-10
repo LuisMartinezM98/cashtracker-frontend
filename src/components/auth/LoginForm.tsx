c436f0f34972a462b2fd075b2c91eb1e7c905c09
LuisMartinezM98 <104807270+LuisMartinezM98@users.noreply.github.com>
2026-06-10 09:33:10 -0600

    feat: LoginForm component finished

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,45 +1,55 @@
-"use client"
+"use client";
+import { useActionState, useEffect } from "react";
+import { toast } from "react-toastify";
+import { useTranslations } from "next-intl";
+import { authenticate } from "@/actions/autenticate-user-action";
 export default function LoginForm() {
+  const [state, dispatch] = useActionState(authenticate, {
+    errors: [],
+  });
 
-    return (
-        <>
-            <form
-                className="mt-14 space-y-5"
-                noValidate
-            >
-                <div className="flex flex-col gap-2">
-                    <label
-                        className="font-bold text-2xl"
-                    >Email</label>
+  const t = useTranslations('RegisterPage');
 
-                    <input
-                        id="email"
-                        type="email"
-                        placeholder="Email de Registro"
-                        className="w-full border border-gray-300 p-3 rounded-lg"
-                        name="email"
-                    />
-                </div>
+  useEffect(() => {
+    if (state.errors) {
+      state.errors.forEach((error) => {
+        toast.error(error);
+      });
+    }
+  }, [state]);
 
-                <div className="flex flex-col gap-2">
-                    <label
-                        className="font-bold text-2xl"
-                    >Password</label>
+  return (
+    <>
+      <form action={dispatch} className="mt-14 space-y-5" noValidate>
+        <div className="flex flex-col gap-2">
+          <label className="font-bold text-2xl">{t('form.email')}</label>
 
-                    <input
-                        type="password"
-                        placeholder="Password de Registro"
-                        className="w-full border border-gray-300 p-3 rounded-lg"
-                        name="password"
-                    />
-                </div>
+          <input
+            id="email"
+            type="email"
+            placeholder={t('form.email_placeholder')}
+            className="w-full border border-gray-300 p-3 rounded-lg"
+            name="email"
+          />
+        </div>
 
-                <input
-                    type="submit"
-                    value='Iniciar Sesión'
-                    className="bg-purple-950 hover:bg-purple-800 w-full p-3 rounded-lg text-white font-black  text-xl cursor-pointer"
-                />
-            </form>
-        </>
-    )
+        <div className="flex flex-col gap-2">
+          <label className="font-bold text-2xl">{t('form.password')}</label>
+
+          <input
+            type="password"
+            placeholder={t('form.password_placeholder')}
+            className="w-full border border-gray-300 p-3 rounded-lg"
+            name="password"
+          />
+        </div>
+
+        <input
+          type="submit"
+          value={t('form.submit')}
+          className="bg-purple-950 hover:bg-purple-800 w-full p-3 rounded-lg text-white font-black  text-xl cursor-pointer"
+        />
+      </form>
+    </>
+  );
 }
