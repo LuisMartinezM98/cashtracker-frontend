b7251273e2d5715895855806dd83cb1fee26801d
LuisMartinezM98 <104807270+LuisMartinezM98@users.noreply.github.com>
2026-06-10 09:31:14 -0600

    feat: Login page finished

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,34 +1,41 @@
-import type { Metadata } from 'next';
-import Link from 'next/link';
-import LoginForm from '@/components/auth/LoginForm';
+import type { Metadata } from "next";
+import Link from "next/link";
+import LoginForm from "@/components/auth/LoginForm";
+import { getTranslations } from "next-intl/server";
+import getToken from "@/auth/token";
+import { redirect } from "next/navigation";
+
+const t = await getTranslations("LoginPage");
 
 export const metadata: Metadata = {
-    title: "CashTrackr - Iniciar Sesión",
-    description: "CashTrackr - Iniciar Sesión"
-}
+  title: t("metadata-title"),
+  description: t("metadata-description"),
+};
 
+// const token = await getToken();
+// if (token) {
+//   redirect("/admin");
+// }
 
 export default function LoginPage() {
-
-    return (
-        <>
-            <h1 className="font-black text-6xl text-purple-950">Iniciar Sesión</h1>
-            <p className="text-3xl font-bold">y controla tus <span className="text-amber-500">finanzas</span></p>
-            <LoginForm />
-            <nav className='mt-10 flex flex-col space-y-4'>
-                <Link
-                    href="/auth/register"
-                    className='text-center text-gray-500'
-                >
-                    ¿No tienes cuenta? Registrate
-                </Link>
-                <Link
-                    href="/auth/forgot-password"
-                    className='text-center text-gray-500'
-                >
-                    ¿Olvidaste tu contraseña? Restablecer
-                </Link>
-            </nav>
-        </>
-    )
+  return (
+    <>
+      <h1 className="font-black text-6xl text-purple-950">{t("title")}</h1>
+      <p className="text-3xl font-bold">
+        {t("subtitle")} <span className="text-amber-500">{t("span")}</span>
+      </p>
+      <LoginForm />
+      <nav className="mt-10 flex flex-col space-y-4">
+        <Link href="/auth/register" className="text-center text-gray-500">
+          {t("singup_link")}
+        </Link>
+        <Link
+          href="/auth/forgot-password"
+          className="text-center text-gray-500"
+        >
+          {t("forgotpassword_link")}
+        </Link>
+      </nav>
+    </>
+  );
 }
