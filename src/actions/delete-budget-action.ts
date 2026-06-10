9dc4015d9a2fca5ffc7b18355dd643dc40e089e2
LuisMartinezM98 <104807270+LuisMartinezM98@users.noreply.github.com>
2026-06-10 09:25:35 -0600

    feat: delete budget action implemented

---
+++ src/actions/delete-budget-action.ts
@@ -0,0 +1,86 @@
+"use server";
+
+import getToken from "@/auth/token";
+import {
+  Budget,
+  ErrorResponseSchema,
+  PasswordValidationSchema,
+  SuccessSchema,
+} from "@/schemas";
+import { revalidatePath } from "next/cache";
+
+type ActionStateType = {
+  errors: string[];
+  success: string;
+};
+
+export async function deleteBudget(
+  budgetId: Budget["id"],
+  prevState: ActionStateType,
+  formData: FormData,
+) {
+  console.log(formData.get("password"));
+  console.log(budgetId);
+
+  const currentPassword = PasswordValidationSchema.safeParse(
+    formData.get("password"),
+  );
+
+  if (!currentPassword.success) {
+    return {
+      errors: currentPassword.error.issues.map((error) => error.message),
+      success: "",
+    };
+  }
+
+  //Comprobar password
+  const token = await getToken();
+  const checkPasswordUrl = `${process.env.NEXT_PUBLIC_API_URL}/api/auth/check-password`;
+  const checkPasswordReq = await fetch(checkPasswordUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({
+      password: currentPassword.data,
+    }),
+  });
+
+  const checkPasswordJson = await checkPasswordReq.json();
+
+  if (!checkPasswordReq.ok) {
+    const { error } = ErrorResponseSchema.parse(checkPasswordJson);
+    return {
+      errors: [error],
+      success: "",
+    };
+  }
+  //Eliminar presupuesto
+  const deleteBudgetUrl = `${process.env.NEXT_PUBLIC_API_URL}/api/budgets/${budgetId}`;
+  const deleteBudgetReq = await fetch(deleteBudgetUrl, {
+    method: "DELETE",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  const deleteBudgetJson = await deleteBudgetReq.json();
+
+  if (!deleteBudgetReq.ok) {
+    const { error } = ErrorResponseSchema.parse(deleteBudgetJson);
+    return {
+      errors: [error],
+      success: "",
+    };
+  }
+
+  revalidatePath('/admin')
+
+  const success = SuccessSchema.parse(deleteBudgetJson);
+
+  return {
+    errors: [],
+    success: success.message,
+  };
+}
