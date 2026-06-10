375f062de445ab394cd0eae001bb6c085c8acc43
LuisMartinezM98 <104807270+LuisMartinezM98@users.noreply.github.com>
2026-06-10 09:38:20 -0600

    feat: ToastNotification component added

---
+++ src/components/ui/ToastNotification.tsx
@@ -0,0 +1,13 @@
+"use client";
+import { ToastContainer } from "react-toastify";
+// import "react-toastify/ReactToastify.min.css"
+
+import React from "react";
+
+export default function ToastNotification() {
+  return (
+    <>
+      <ToastContainer />
+    </>
+  );
+}
